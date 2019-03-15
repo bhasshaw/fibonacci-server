ef2da3628612e67c3a4e7a992230be1b0d0afead
Bryce Hasshaw <bhasshaw@gmail.com>
2019-03-14 18:14:24 -0700

    added endpoint

--- main.go
+++ main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
 	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func Fibonacci(n int) []int {
+
+	if n < 2 {
+		return []int{0}
+	}
+	f := make([]int, n, n+1)
+	f[0] = 0
+	f[1] = 1
+	for i := 2; i <= n-1; i++ {
+		f[i] = f[i-1] + f[i-2]
+	}
+	return f
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func handleFibonacci(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fib := ps.ByName("number")
+	number, _ := strconv.Atoi(fib)
+	returned := Fibonacci(number)
+	json.NewEncoder(w).Encode(returned)
 }
 
 func main() {
 	router := httprouter.New()
-	router.GET("/api", Index)
-	router.GET("/api/hello/:name", Hello)
+	router.GET("/api/fibonacci/:number", handleFibonacci)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
